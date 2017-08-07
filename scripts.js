b0c4ead2c2b4102a4fc420118ebe8189b21c0029
Sean Stevens <sstevens88@gmail.com>
2017-08-07 00:27:21 -0700

    expanded palette and finished base functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,29 @@
-$('.grid-element').click(function() {
-    $(this).css({'background': 'red',
-                 'border-color': 'red'});
+var curColor;
+var curDown;
+
+$('.grid-element').on({mousedown: function(e) {
+                        e.preventDefault();
+                        curDown = true;
+                        $(this).css({'background': curColor,
+                                     'border-color': curColor});
+                        $('.grid-element').mouseenter(function() {
+                            if (curDown === true) {
+                                $(this).css({'background': curColor,
+                                            'border-color': curColor});
+                                }
+                            });
+                        },
+                        mouseup: function() {
+                            curDown = false;
+                    }
 });
+
+$('.canvas').mouseleave(function() {
+    curDown = false;
+});
+
+$('.swatch').click(function(e) {
+    e.preventDefault();
+    curColor = $(this).css('background-color');
+});
+
